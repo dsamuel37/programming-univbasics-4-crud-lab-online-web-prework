7851a78f5d57e932b280c08f7cd5d4f3fa893a76
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 17:45:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array = ["coffee", "cookies", "tea", "scone"]
-array << "muffins"
+add_element_to_end_of_array = ["coffee", "cookies", "tea", "scone"]
+add_element_to_start_of_array << "muffins"
 end
 
 def add_element_to_start_of_array(array, element)
