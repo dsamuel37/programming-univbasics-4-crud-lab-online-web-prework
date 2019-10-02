a052c7a2bfad412addf52ced029e9cb9b91da90d
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 17:54:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_array = ["I", "am", "really", "learning", "arrays!"]
-  my_array.pop("arrays!")
+  arrays = my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
