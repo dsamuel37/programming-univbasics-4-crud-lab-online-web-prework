434b2f1fa993e9118083d144ef3d37ec395ff736
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 17:42:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << "adding"
+  add_element_to_end_of_array = (array, element)
+  add_element_to_end_of_array << add
 end
 
 def add_element_to_start_of_array(array, element)
