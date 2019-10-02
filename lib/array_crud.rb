30f46ec86e6aa1269043b1ff49d917f86440ec17
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 17:50:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 my_array = ["coffee", "cookies", "tea", "scone"]
-    my_array.push ("muffins")
+    my_array.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
