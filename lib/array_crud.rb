e0c5c67955a912a3cd95a8e5be514c13764df11e
Destiny Samuel <destinysamuel37@gmail.com>
2019-10-02 17:47:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array = ["wow", "I", "am", "really", "learning"]
-    element = "arrays!"
+my_array = ["wow", "I", "am", "really", "learning"]
+    my_array.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
